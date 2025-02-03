a393973cc94992c10bb8b8760aded1c763b606ed
Carlos Calderón <carloscalderon04@hotmail.com>
2025-02-03 15:13:57 -0500

    Created main.py file

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+# from app.database  import engine
+# from app.models import todo_model
+# from app.routers import todo_router
+
+app = FastAPI()
+
+@app.get("/")
+async def hello_world():
+    return {"message": "Hola Mundo"}
+
+# todo_model.Base.metadata.create_all(bind=engine)
+
+# app.include_router(todo_router.router)
